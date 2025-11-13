c9d2ddb93791c8b0d0156c0ef6615424e9278025
Tran Khoa <tdkhoa2409@mgail.com>
2025-11-13 07:35:40 +0700

    fix layout

--- app/(teacher)/questions/new/page.tsx
+++ app/(teacher)/questions/new/page.tsx
@@ -1,10 +1,6 @@
-import { DashboardLayout } from "@/components/dashboard-layout"
-import { QuestionForm } from "@/components/question-form"
+import { DashboardLayout } from "@/components/dashboard-layout";
+import { QuestionForm } from "@/components/question-form";
 
 export default function NewQuestionPage() {
-  return (
-    <DashboardLayout role="teacher">
-      <QuestionForm />
-    </DashboardLayout>
-  )
+  return <QuestionForm />;
 }
